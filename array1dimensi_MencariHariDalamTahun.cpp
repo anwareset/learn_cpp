d2ce500c162533b51c932f06878f0c1da2c6e57d
anwareset <anwareset@gmail.com>
2017-04-11 20:22:26 +0700

    Add files via upload

---
+++ array1dimensi_MencariHariDalamTahun.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int jumh[12]=
+	{		
+		31,29,31,30,31,30,31,30,31,30,31,30
+	};
+	int tgl,bln,jmlh=0;
+	cout<<"Program Menari Hari di Tahun 2008\n";
+	cout<<"Masukkan tgl = ";cin>>tgl;
+	cout<<"Masukkan bln = ";cin>>bln;
+	for(int i=0;i<(bln-i);i++){
+		jmlh=jmlh+jumh[i];
+	}
+	jmlh=jmlh+tgl;
+	cout<<"\n\nJumlah hr = "<<jmlh;
+	int sisa;
+	sisa = jmlh % 7;
+	cout<<"\nTanggal "<<tgl<<"\nBulan "<<bln<<"\nadalah Hari = ";
+	if (sisa==0)
+		cout<<"Senin";
+	else if (sisa==1)
+		cout<<"Selasa";
+		else if (sisa==2)
+			cout<<"Rabu";
+			else if(sisa==3)
+				cout<<"Kamis";
+				else if (sisa == 4)
+					cout<<"Jumat";
+						else if(sisa==5)
+							cout<<"Sabtu";
+							else if(sisa==6)
+								cout<<"Minggu";
+}
