b3d19cec74e817cc76e86a2faa65e8e909cc1b57
anwareset <anwareset@gmail.com>
2017-05-11 23:08:12 +0700

    Update MencariArrayTerbesarDanTerkecil.cpp

--- MencariArrayTerbesarDanTerkecil.cpp
+++ MencariArrayTerbesarDanTerkecil.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	int array[3][3]={10,100,78,11,5,43,56,8,120};
-	int terkecil=120,terbesar=0; //variabel penampung
+	int terkecil=120,terbesar=0; //variabel penampung parameter if
 	cout<<"Nilai terbesar & terkecil dari array :\n\n";
 	for(int x=0;x<3;x++){
 		for(int y=0;y<3;y++){
